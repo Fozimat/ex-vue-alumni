9040ca5ee6dcc13992084d8358643ec2f7e5cfe8
Fozimat <fozimata@gmail.com>
2021-10-27 22:10:00 +0700

    Show data

--- vue-frontend/src/components/AlumniTable.vue
+++ vue-frontend/src/components/AlumniTable.vue
@@ -3,45 +3,29 @@
     <section id="pricing" class="pricing">
       <div class="container" data-aos="fade-up">
         <h2 class="title-alumni">Daftar Alumni</h2>
-        <table class="table table-bordered table-hover">
+        <table class="table table-hover">
           <thead>
             <tr>
               <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+              <th scope="col">NISN</th>
+              <th scope="col">Nama</th>
+              <th scope="col">Tahun Masuk</th>
+              <th scope="col">Tahun Lulus</th>
+              <th scope="col">Aksi</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
+            <tr v-for="(result, index) in results" :key="result.id">
+              <th scope="row">{{ index + 1 }}</th>
+              <td>{{ result.nisn }}</td>
+              <td>{{ result.nama }}</td>
+              <td>{{ result.tahun_masuk }}</td>
+              <td>{{ result.tahun_lulus }}</td>
+              <td>
+                <a href="#" class="btn btn-primary btn-sm"
+                  ><i class="bi bi-eye-fill margin-see"></i>See Detail</a
+                >
+              </td>
             </tr>
           </tbody>
         </table>
@@ -54,10 +38,33 @@
 .title-alumni {
   margin-top: -3rem;
 }
+.margin-see {
+  margin-right: 0.3rem !important;
+}
 </style>
 
 <script>
+import axios from "axios";
 export default {
   name: "AlumniTable",
+  data() {
+    return {
+      results: [],
+    };
+  },
+  created() {
+    this.showData();
+  },
+  methods: {
+    async showData() {
+      try {
+        const url = "http://127.0.0.1:5000/alumni";
+        const response = await axios.get(url);
+        this.results = response.data.data;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
 };
 </script>
